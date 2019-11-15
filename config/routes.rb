3c3f044d65ebd08be2345e597a8a6e956b0c00d0
thsvr <vr.ths.zd@gmail.com>
2019-11-15 17:21:06 -0300

    controller to Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # default get 'pages/home'
+  # default get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
