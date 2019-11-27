7691f834c0833cf4678de3960e3b0b614756add8
thsvr <vr.ths.zd@gmail.com>
2019-11-27 12:16:22 -0300

    refactor create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,12 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to article_path(@article)
+         if @article.save
+            flash [:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+         else
+            render 'new'
     end
 
     private
