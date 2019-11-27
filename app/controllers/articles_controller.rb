32dc1ede762b1ff6b8366c41351703aae064eafa
thsvr <vr.ths.zd@gmail.com>
2019-11-27 12:29:40 -0300

    adding msg to display when an article was not  successfully creadted

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,11 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
          if @article.save
-            flash [:notice] = "Article was successfully created"
+            flash[:notice] = "Article was successfully created"
             redirect_to article_path(@article)
          else
             render 'new'
+         end
     end
 
     private
