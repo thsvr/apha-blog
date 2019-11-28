72e2694849431e96868fc8a46420356c26cc86e0
thsvr <vr.ths.zd@gmail.com>
2019-11-28 20:59:58 -0300

    FINALLY CONNECTING BOOTSTRAP WITH RAISL

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
+//= require rails-ujs 
 //= require jquery
-//= require bootstrap-sprockets
-//= require rails-ujs   
 //= require turbolinks
-//= require_tree .
