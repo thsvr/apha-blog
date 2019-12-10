4cbbeaa40b8df9831ab1c4ccd6a259f94e157db3
thsvr <vr.ths.zd@gmail.com>
2019-12-10 19:37:18 -0300

    adding some style to navbar

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'bootstrap'
 gem 'sass-rails'
+gem 'jquery-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
-//= require rails-ujs 
-//= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
 //= require turbolinks
+//= require_tree .
+
+
