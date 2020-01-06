aa1896b11a1ea9878d156c809a0eab88a67268d1
thsvr <vr.ths.zd@gmail.com>
2020-01-06 13:25:31 -0300

    added association to connect user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    belogs_to :user
     validates :title, presence: true, length: { mininum: 3, maximum: 50 }
     validates :description, presence: true, length: { mininum: 10, maximum: 300 }
 end
