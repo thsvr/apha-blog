797dd6f80f49f87e10af795f05cd5ba160b8b66b
thsvr <vr.ths.zd@gmail.com>
2020-01-06 13:49:29 -0300

    fixed regex and belongs_to association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    belogs_to :user
+    belongs_to :user
     validates :user_id, presence: true
     validates :title, presence: true, length: { mininum: 3, maximum: 50 }
     validates :description, presence: true, length: { mininum: 10, maximum: 300 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     validates :username, presence: true,
               uniqueness: { case_sensitive: false},
               length: { in: 3..25 }
-    VALID_EMAIL_REGEX: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true,
              uniqueness: { case_sensitive: false},
              length: { maximum: 105 },
